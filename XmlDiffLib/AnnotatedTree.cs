d87b8da40cf313d9a7e56c67ab20ed68f729f3a9
krtaborn <kory.taborn@gmail.com>
2016-09-12 22:37:52 -0500

    Begins translation to Zhang-Shasha edit tree distance algorithm Uses generic node interface to potentially allow future use of other hierarchically structured data

---
+++ XmlDiffLib/AnnotatedTree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace XmlDiffLib
+{
+  internal class AnnotatedTree<T>
+  {
+    public ITreeNode<T> Root;
+    public List<ITreeNode<T>> Nodes;
+    public List<ITreeNode<T>> Ids;
+    public List<ITreeNode<T>> LeftMostDescendants;
+  }
+}
---
+++ XmlDiffLib/ITreeNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XmlDiffLib
+{
+  public interface ITreeNode<T> : IEnumerable<ITreeNode<T>>
+  {
+    List<ITreeNode<T>> Children { get; }
+    string Label { get; }
+    ITreeNode<T> AddChild(ITreeNode<T> node, bool before = false);
+    ITreeNode<T> GetChild(string label);
+  }
+}
